e676d59fcba0a4e1726266f9e8946d7b6b27ab34
Merlin Buczek <merlin@frenmail.com>
2022-12-27 13:50:14 +1300

    Add new endpoints

--- src/plugins/bitails.js
+++ src/plugins/bitails.js
@@ -3,7 +3,7 @@ const MinercraftClass = require("../classes/minercraft_class");
 module.exports = class Plugin extends MinercraftClass {
   constructor(params) {
     const url = "https://mapi.bitails.net/";
-    super({ ...params, url });
+    super({ ...params, url, name: "bitails" });
   }
 
   static getName() {
--- src/plugins/gorillapool.js
+++ src/plugins/gorillapool.js
@@ -3,7 +3,7 @@ const MinercraftClass = require("../classes/minercraft_class");
 module.exports = class Plugin extends MinercraftClass {
   constructor(params) {
     const url = "https://mapi.gorillapool.io/";
-    super({ ...params, url });
+    super({ ...params, url, name: "gorillapool" });
   }
 
   static getName() {
